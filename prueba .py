be89a7d6425e01c17e1ccdb1a53ac2974491962b
1Oscar1Leon1 <oaleon06@misena.edu.co>
2022-08-08 16:47:38 -0500

    Version 4.0

--- prueba .py
+++ prueba .py
@@ -11,6 +11,7 @@ print(type(item1_price_total)) # int'''
 
 class Item:
     pay_rate = 0.8 #They pay rate after 20% discount
+    all = []
     def __init__(self, name: str,price:float, quantity=0):
         #Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
@@ -21,6 +22,8 @@ class Item:
         self.price = price
         self.quantity = quantity
         
+        #Action to execute
+        Item.all.append(self)
 
     def calculate_total_price(self):
         return self.price * self.quantity
@@ -29,11 +32,11 @@ class Item:
         self.price = self.price * self.pay_rate
 
 
-item1 = Item("Phone",100,1)
-item1.apply_discount()
-print(item1.price)
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-item2 =Item("Laptop",1000,3)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+for instance in Item.all:
+    print(instance.name)
